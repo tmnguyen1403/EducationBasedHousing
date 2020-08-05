f18bbeb72def316a1559eb96344e8e7f0c7ad862
Minh Nguyen <tmnguyen1403@gmail.com>
2020-08-05 02:26:58 -0500

    Add ImagePicker

--- App.js
+++ App.js
@@ -9,13 +9,13 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 //TEST
-import TestServerData from './components/TestServerData'
+import ImagePicker from './components/TestImagePicker'
 export default function App() {
 //
   return (
 		<Provider store={createStore(reducer)}>
 			<View style={[styles.container]}>
-				<BottomTabNavigator/>
+				<ImagePicker/>
 			</View>
 		</Provider>
   );
