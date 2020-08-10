1d0653f6f7252807673eb5aa40f37c8b92f9c5e0
Minh Nguyen <tmnguyen1403@gmail.com>
2020-08-10 04:06:46 -0500

    Switch to heroku server

--- utils/configs.js
+++ utils/configs.js
@@ -1,4 +1,4 @@
-//const SERVER_HOST = "https://guarded-brook-45524.herokuapp.com/"
-const SERVER_HOST = null
+const SERVER_HOST = "https://guarded-brook-45524.herokuapp.com/"
+//const SERVER_HOST = null
 export const HOST = SERVER_HOST || "http://localhost:3000/"
 export const IMAGE_HOST = HOST + 'uploads/'
